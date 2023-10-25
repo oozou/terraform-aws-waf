599637a4b6d82db1d7338edb779d9d1eb757d234
xshot9011 <40098197+xshot9011@users.noreply.github.com>
2023-10-25 12:47:16 +0700

    feat: update waf module supporting or statements

--- main.tf
+++ main.tf
@@ -389,7 +389,295 @@ resource "aws_wafv2_web_acl" "this" {
         dynamic "or_statement" {
           for_each = rule.value.expression_type == "or-statements" ? [1] : []
           content {
+            dynamic "statement" {
+              for_each = rule.value.statements
+              iterator = user_defined_statement
+              content {
+                # replace `rule.value.statements[0]` with `user_defined_statement.value`
+                # Add `&& lookup(user_defined_statement.value, "is_negated_statement", false) == false` to condition for non not_statement
+                /* -------------------------------------------------------------------------- */
+                /*      (START): SINGLE MATCH STATEMEN (2) [is_negated_statement = false]     */
+                /* -------------------------------------------------------------------------- */
+                ## Originates from IP address
+                dynamic "ip_set_reference_statement" {
+                  for_each = user_defined_statement.value.inspect == local.originate_from_an_ip_addresses_in && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                  content {
+                    arn = aws_wafv2_ip_set.this[user_defined_statement.value.ip_set_key].arn
+                  }
+                }
+                ## Originates from country
+                #### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#country_codes
+                dynamic "geo_match_statement" {
+                  for_each = user_defined_statement.value.inspect == local.originate_from_a_country_in && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                  content {
+                    country_codes = user_defined_statement.value.country_codes
+                  }
+                }
+                ## Labels
+                #### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#label_match_statement
+                dynamic "label_match_statement" {
+                  for_each = user_defined_statement.value.inspect == local.has_a_label && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                  content {
+                    scope = user_defined_statement.value.scope
+                    key   = user_defined_statement.value.key
+                  }
+                }
 
+                # Request component (byte_match_statement, size_constraint_statement, sqli_match_statement)
+                ## byte_match_statement (String Macth Condition)
+                #### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#byte_match_statement
+                dynamic "byte_match_statement" {
+                  for_each = contains(local.request_component_dynamic_blocks, user_defined_statement.value.inspect) && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                  content {
+                    positional_constraint = user_defined_statement.value.positional_constraint
+                    search_string         = user_defined_statement.value.search_string
+                    field_to_match {
+                      # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#single_header
+                      dynamic "single_header" {
+                        for_each = user_defined_statement.value.inspect == local.single_header && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                        content {
+                          name = user_defined_statement.value.header_name
+                        }
+                      }
+
+                      # TODO: support all headers with oversizing handler
+                      # dynamic "headers" {
+                      #   for_each = user_defined_statement.value.inspect == local.all_headers && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                      #   content {
+
+                      #   }
+                      # }
+
+                      # TODO: support cookies with oversizing handler
+                      # dynamic "cookies" {
+                      #   for_each = user_defined_statement.value.inspect == local.cookies && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                      #   content {
+
+                      #   }
+                      # }
+
+                      # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#single_query_argument
+                      dynamic "single_query_argument" {
+                        for_each = user_defined_statement.value.inspect == local.single_query_parameter && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                        content {
+                          name = user_defined_statement.value.query_string_name
+                        }
+                      }
+
+                      # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#all_query_arguments
+                      dynamic "all_query_arguments" {
+                        for_each = user_defined_statement.value.inspect == local.all_query_parameters && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                        content {
+
+                        }
+                      }
+
+                      # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#uri_path
+                      dynamic "uri_path" {
+                        for_each = user_defined_statement.value.inspect == local.uri_path && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                        content {
+
+                        }
+                      }
+
+                      # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#query_string
+                      dynamic "query_string" {
+                        for_each = user_defined_statement.value.inspect == local.query_string && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                        content {
+
+                        }
+                      }
+
+                      # TODO: support body, json with oversizing handler
+                      # dynamic "body" {
+                      #   for_each = user_defined_statement.value.inspect == local.body && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                      #   content {
+
+                      #   }
+                      # }
+
+                      # TODO: support json_body with oversizing handler
+                      ## Can use input the same as body
+                      # dynamic "json_body" {
+                      #   for_each = user_defined_statement.value.inspect == local.json_body && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                      #   content {
+
+                      #   }
+                      # }
+
+                      dynamic "method" {
+                        for_each = user_defined_statement.value.inspect == local.http_method && lookup(user_defined_statement.value, "is_negated_statement", false) == false ? [1] : []
+                        content {
+
+                        }
+                      }
+
+                      # Not support for terraform at 2023-06-19
+                      # dynamic "header_order" {
+
+                      # }
+                    }
+
+                    #### TODO: support text transformation
+                    text_transformation {
+                      priority = 0
+                      type     = "NONE"
+                    }
+                  }
+                }
+                #### TODO:
+                #### support size_constraint_statement (Size Match Condition)
+                #### support sqli_match_statement (Attack Match Condition)
+                /* -------------------------------------------------------------------------- */
+                /*       (END): SINGLE MATCH STATEMEN (2) [is_negated_statement = false]      */
+                /* -------------------------------------------------------------------------- */
+                # is_negated_statement = true
+                dynamic "not_statement" {
+                  for_each = lookup(user_defined_statement.value, "is_negated_statement", false) == true ? [1] : [] # Force false
+                  content {
+                    statement {
+                      # replace `rule.value.statements[0]` with `user_defined_statement.value`
+                      /* -------------------------------------------------------------------------- */
+                      /*      (START): SINGLE MATCH STATEMEN (2) [is_negated_statement = true]      */
+                      /* -------------------------------------------------------------------------- */
+                      ## Originates from IP address
+                      dynamic "ip_set_reference_statement" {
+                        for_each = user_defined_statement.value.inspect == local.originate_from_an_ip_addresses_in ? [1] : []
+                        content {
+                          arn = aws_wafv2_ip_set.this[user_defined_statement.value.ip_set_key].arn
+                        }
+                      }
+                      ## Originates from country
+                      #### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#country_codes
+                      dynamic "geo_match_statement" {
+                        for_each = user_defined_statement.value.inspect == local.originate_from_a_country_in ? [1] : []
+                        content {
+                          country_codes = user_defined_statement.value.country_codes
+                        }
+                      }
+                      ## Labels
+                      #### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#label_match_statement
+                      dynamic "label_match_statement" {
+                        for_each = user_defined_statement.value.inspect == local.has_a_label ? [1] : []
+                        content {
+                          scope = user_defined_statement.value.scope
+                          key   = user_defined_statement.value.key
+                        }
+                      }
+
+                      # Request component (byte_match_statement, size_constraint_statement, sqli_match_statement)
+                      ## byte_match_statement (String Macth Condition)
+                      #### https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#byte_match_statement
+                      dynamic "byte_match_statement" {
+                        for_each = contains(local.request_component_dynamic_blocks, user_defined_statement.value.inspect) ? [1] : []
+                        content {
+                          positional_constraint = user_defined_statement.value.positional_constraint
+                          search_string         = user_defined_statement.value.search_string
+                          field_to_match {
+                            # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#single_header
+                            dynamic "single_header" {
+                              for_each = user_defined_statement.value.inspect == local.single_header ? [1] : []
+                              content {
+                                name = user_defined_statement.value.header_name
+                              }
+                            }
+
+                            # TODO: support all headers with oversizing handler
+                            # dynamic "headers" {
+                            #   for_each = user_defined_statement.value.inspect == local.all_headers ? [1] : []
+                            #   content {
+
+                            #   }
+                            # }
+
+                            # TODO: support cookies with oversizing handler
+                            # dynamic "cookies" {
+                            #   for_each = user_defined_statement.value.inspect == local.cookies ? [1] : []
+                            #   content {
+
+                            #   }
+                            # }
+
+                            # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#single_query_argument
+                            dynamic "single_query_argument" {
+                              for_each = user_defined_statement.value.inspect == local.single_query_parameter ? [1] : []
+                              content {
+                                name = user_defined_statement.value.query_string_name
+                              }
+                            }
+
+                            # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#all_query_arguments
+                            dynamic "all_query_arguments" {
+                              for_each = user_defined_statement.value.inspect == local.all_query_parameters ? [1] : []
+                              content {
+
+                              }
+                            }
+
+                            # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#uri_path
+                            dynamic "uri_path" {
+                              for_each = user_defined_statement.value.inspect == local.uri_path ? [1] : []
+                              content {
+
+                              }
+                            }
+
+                            # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/wafv2_web_acl.html#query_string
+                            dynamic "query_string" {
+                              for_each = user_defined_statement.value.inspect == local.query_string ? [1] : []
+                              content {
+
+                              }
+                            }
+
+                            # TODO: support body, json with oversizing handler
+                            # dynamic "body" {
+                            #   for_each = user_defined_statement.value.inspect == local.body ? [1] : []
+                            #   content {
+
+                            #   }
+                            # }
+
+                            # TODO: support json_body with oversizing handler
+                            ## Can use input the same as body
+                            # dynamic "json_body" {
+                            #   for_each = user_defined_statement.value.inspect == local.json_body ? [1] : []
+                            #   content {
+
+                            #   }
+                            # }
+
+                            dynamic "method" {
+                              for_each = user_defined_statement.value.inspect == local.http_method ? [1] : []
+                              content {
+
+                              }
+                            }
+
+                            # Not support for terraform at 2023-06-19
+                            # dynamic "header_order" {
+
+                            # }
+                          }
+
+                          #### TODO: support text transformation
+                          text_transformation {
+                            priority = 0
+                            type     = "NONE"
+                          }
+                        }
+                      }
+                      #### TODO:
+                      #### support size_constraint_statement (Size Match Condition)
+                      #### support sqli_match_statement (Attack Match Condition)
+                      /* -------------------------------------------------------------------------- */
+                      /*       (END): SINGLE MATCH STATEMEN (2) [is_negated_statement = true]       */
+                      /* -------------------------------------------------------------------------- */
+                    }
+                  }
+                }
+              }
+            }
           }
         }
 
